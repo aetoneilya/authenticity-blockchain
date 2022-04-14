5f8313427b1ddd7f13945f5a781332f3b752da54
aetoneilya <udalov216@gmail.com>
2022-04-15 00:14:11 +0700

    front end start

---
+++ front_end/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { DAppProvider, ChainId } from '@usedapp/core';
+import { Header } from "./components/Header"
+import { Container } from "@material-ui/core"
+
+function App() {
+    return (
+        <DAppProvider config={{
+            supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+        }}>
+            <Header />
+            <Container maxWidth="md">
+                <div>Hi!</div>
+            </Container>
+
+        </DAppProvider>
+    );
+}
+
+export default App;
